2210ab1e890d945c893e752435538aa27c93f6a3
Tormod Aase <tormod.aase@gmail.com>
2018-05-09 23:11:58 +0900

    added gnuplot code

--- trialDivision.c
+++ trialDivision.c
@@ -21,6 +21,11 @@ void trialDivision(int n) {
 }
 
 int main(int argc, char **argv) {
+	/*gnuplot*/
+	FILE *gnuplot = popen("gnuplot -persist", "w");
+	fprintf(gnuplot, "set style line 1 lc rgb '#0060ad' lt 1 lw 2 pt 7 ps 1.5\n");
+	fprintf(gnuplot, "plot '-' with linespoints ls 1\n");
+
 	for (int weight=1; weight<=20; weight++) {
 		syscall(sched_setweight, getpid(), weight);
 		
@@ -33,6 +38,13 @@ int main(int argc, char **argv) {
 		}
 		clock_t end = clock();
 		double time_spent = (double)(end-begin)/CLOCKS_PER_SEC;
+		
+		fprintf(gnuplot, "%d %g\n", weight, time_spent); //gnuplot
+		
 		printf("Execution time with n=%d: %f\n", n, time_spent);
 	}
+	
+	/*gnuplot*/
+	fprintf(gnuplot, "e\n");
+	fflush(gnuplot);
 }
